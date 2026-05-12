e9aaddf1e2da1ca0de6481037f57264990497799
Salohy <salohiniainarak@gmail.com>
2026-05-12 12:47:10 +0300

    Add a link that open the profile.

--- dashboard.php
+++ dashboard.php
@@ -5,6 +5,7 @@ require_once "middleware/auth.php";
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="assets/css/dashboard.css">
     <title>Dashboard</title>
 </head>
 <body>
@@ -15,6 +16,10 @@ require_once "middleware/auth.php";
 </h1>
 
 <p>You are logged in.</p>
+
+<a class="profile-link" href="profile/profile.php">
+    My profile
+</a>
 
 <a href="auth/logout.php">
     Logout
