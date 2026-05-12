e6039187704583448347ef400f03eaae4582c9b8
Salohy <salohiniainarak@gmail.com>
2026-05-12 13:48:34 +0300

    Add a code to avoid a session duplicate.

--- middleware/auth.php
+++ middleware/auth.php
@@ -1,7 +1,11 @@
 <?php
-session_start();
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Check if user is logged in
 if (!isset($_SESSION["user_id"])) {
-    header("Location: /auth/login.php");
-    exit();
+    header("Location: ../auth/login.php");
+    exit;
 }
