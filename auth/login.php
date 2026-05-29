94574ec9c045cc0a38c3b1a175c071a4319f669d
Salohy <salohiniainarak@gmail.com>
2026-05-29 10:49:14 +0300

    Change link to dashboard.

--- auth/login.php
+++ auth/login.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $_SESSION["user_id"] = $user["id"];
         $_SESSION["username"] = $user["username"];
 
-        header("Location: ../dashboard.php");
+        header("Location: ../dashboard/dashboard.php");
         exit();
 
     } else {
